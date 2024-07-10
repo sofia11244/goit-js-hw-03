6f772544aa0e9a6e2dad0ea10edaf2e1532f2f0e
sofia11244 <denizsofia03@gmail.com>
2024-07-10 13:44:33 +0300

    task-3 error fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,17 @@
 function filterArray(numbers, value) {
-  const filteredNumbers = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      filteredNumbers.push(numbers[i]);
+  const newArray = [];
+
+  for (const number of numbers) {
+    if (number > value) {
+      newArray.push(number);
     }
   }
-  return filteredNumbers;
+
+  return newArray;
 }
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([10, 20, 30, 40, 50], 25)); // [30, 40, 50]
-console.log(filterArray([5, 4, 3, 2, 1], 2)); // [5, 4, 3]
-console.log(filterArray([12, 5, 8, 130, 44], 10)); // [12, 130, 44]
-console.log(filterArray([-1, -2, -3, -4, -5], -3)); // [-1, -2]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
